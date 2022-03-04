1489db470dd253f0ea345cbd27652e8efe4a4f51
Pierre Smit <pierre.smit@outlook.com>
2022-03-05 00:40:45 +0200

    To Version:0.1.16

--- mybuild.ps1
+++ mybuild.ps1
@@ -1,35 +1,39 @@
 function mybuild {
-PARAM(
-	[Parameter(Mandatory = $true)]
-	[ValidateSet('Combine', 'Build')]
-	[string]$Update
-)
+	PARAM(
+		[Parameter(Mandatory = $true)]
+		[ValidateSet('Combine', 'Build')]
+		[string]$Update
+	)
 
-Get-Module PSToolKit | Remove-Module -Force
-Import-Module 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\Public\Set-PSProjectFiles.ps1' -Force
-try {
-	Copy-Item 'C:\Program Files\PSToolKit\Config\*' -Destination 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\Private' -Force -ErrorAction Stop
-} catch {throw "Cant copy config files"}
-
-
-if ($Update -like 'Build') {
+	Get-Module PSToolKit | Remove-Module -Force
+	Import-Module 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\Public\Set-PSProjectFiles.ps1' -Force
 	try {
-		Remove-Item 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\docs' -Force -Recurse -ErrorAction Stop
-		Remove-Item 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Output' -Force -Recurse -ErrorAction Stop
-		Remove-Item 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Issues.xlsx' -Force -ErrorAction Stop
-	} catch {Write-Warning 'cant delete files'}
-	Set-PSProjectFiles -ModulePSM1 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\PSToolKit.psm1' -VersionBump Build -mkdocs gh-deploy
-} else {
-	Set-PSProjectFiles -ModulePSM1 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\PSToolKit.psm1' -VersionBum CombineOnly -mkdocs serve
-}
+		Copy-Item 'C:\Program Files\PSToolKit\Config\*' -Destination 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\Private' -Force -ErrorAction Stop
+	} catch {throw "Cant copy config files`nMessage:$($_.Exception.Message)`nItem:$($_.Exception.ItemName)"}
 
-$newmod = ((Get-ChildItem -Path 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Output\') | Sort-Object -Property Name -Descending)[0]
-Get-ChildItem -Directory 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' | Compress-Archive -DestinationPath 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit\oldmodule.zip' -Update
-Get-ChildItem -Directory 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' | Remove-Item -Recurse -Force
-Copy-Item -Path $newmod.FullName -Destination 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit\' -Force -Verbose -Recurse
 
-Set-Location 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit'
-git add --all
-git commit --all -m "To Version:$((Get-ChildItem D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Output).name.ToString())"
-git push
+	if ($Update -like 'Build') {
+		try {
+			Remove-Item 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\docs' -Force -Recurse -ErrorAction Stop
+			Remove-Item 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Output' -Force -Recurse -ErrorAction Stop
+			Remove-Item 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Issues.xlsx' -Force -ErrorAction Stop
+		} catch {Write-Warning "Cant delete files`nMessage:$($_.Exception.Message)`nItem:$($_.Exception.ItemName)"}
+		Set-PSProjectFiles -ModulePSM1 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\PSToolKit.psm1' -VersionBump Build -mkdocs gh-deploy
+	} else {
+		Set-PSProjectFiles -ModulePSM1 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\PSToolKit\PSToolKit.psm1' -VersionBum CombineOnly -mkdocs serve
+	}
+
+	try {
+		$newmod = ((Get-ChildItem -Path 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Output\') | Sort-Object -Property Name -Descending)[0]
+		Get-ChildItem -Directory 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' | Compress-Archive -DestinationPath 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit\oldmodule.zip' -Update
+		Get-ChildItem -Directory 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit' | Remove-Item -Recurse -Force
+		Copy-Item -Path $newmod.FullName -Destination 'C:\Program Files\WindowsPowerShell\Modules\PSToolKit\' -Force -Recurse
+	} catch {Write-Warning "Unable to copy the new module `nMessage:$($_.Exception.Message)`nItem:$($_.Exception.ItemName)"}
+
+
+	Set-Location 'D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit'
+	Start-Sleep 15
+	git add --all
+	git commit --all -m "To Version:$((Get-ChildItem D:\SharedProfile\CloudStorage\Dropbox\#Profile\Documents\PowerShell\ProdModules\PSToolKit\Output).name.ToString())"
+	git push
 }
