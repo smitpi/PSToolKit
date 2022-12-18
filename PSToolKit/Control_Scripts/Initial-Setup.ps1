21f4fec13c79fffb823a25ec808088df0c349f0f
smitpi <pierre.smit@outlook.com>
2022-12-18 06:33:25 +0200

    Update Initial-Setup.ps1

--- PSToolKit/Control_Scripts/Initial-Setup.ps1
+++ PSToolKit/Control_Scripts/Initial-Setup.ps1
@@ -80,7 +80,22 @@ if (-not(Test-Path "$($PSDownload.fullname)\BaseApps.tmp")) {
 			Invoke-Reboot -ComputerName $env:COMPUTERNAME -Verbose
 		} else {Write-Host 'Not Required' -ForegroundColor Green}
 		
-		Install-PSPackageManAppFromList -ListName BaseApps -GitHubUserID $GitHubUserID -GitHubToken $GitHubToken
+		### HACK Workaround choco / boxstarter path too long error
+		## https://github.com/chocolatey/boxstarter/issues/241
+		$ChocoCachePath = "$($PSDownload.fullname)\chocolatey"
+		New-Item -Path $ChocoCachePath -ItemType Directory -Force
+
+		$cup = 'choco upgrade --cacheLocation="$ChocoCachePath"'
+
+		Invoke-Expression "$cup bandizip"
+		Invoke-Expression "$cup cascadia-code-nerd-font"
+		Invoke-Expression "$cup cascadiacodepl"
+		Invoke-Expression "$cup GoogleChrome"
+		Invoke-Expression "$cup microsoft-edge"
+		Invoke-Expression "$cup microsoft-windows-terminal"
+		Invoke-Expression "$cup pwsh"
+
+		#Install-PSPackageManAppFromList -ListName BaseApps -GitHubUserID $GitHubUserID -GitHubToken $GitHubToken
 		New-Item "$($PSDownload.fullname)\LabSetup.tmp" -ItemType file -Force | Out-Null
 	} catch {Write-Warning "Error: Message:$($Error[0])"}
 }
