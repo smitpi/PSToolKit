cc7de461eb4f578c5457da925a201f9de39587e9
smitpi <pierre.smit@outlook.com>
2022-12-18 05:00:41 +0200

    f

--- PSToolKit/Control_Scripts/Initial-Setup.ps1
+++ PSToolKit/Control_Scripts/Initial-Setup.ps1
@@ -72,12 +72,15 @@ Start-PSToolkitSystemInitialize -GitHubUserID $GitHubUserID -GitHubToken $GitHub
 Remove-Item $full.FullName
 
 if (-not(Test-Path "$($PSDownload.fullname)\BaseApps.tmp")) {
-	Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Base Apps`n" -ForegroundColor Cyan
-	Write-Host '[Checking] ' -NoNewline -ForegroundColor Yellow; Write-Host 'Pending Reboot: ' -ForegroundColor Cyan -NoNewline
-	if (Test-PendingReboot -ComputerName $env:COMPUTERNAME) {Invoke-Reboot}
-	else {Write-Host 'Not Required' -ForegroundColor Green}
-	Install-PSPackageManAppFromList -ListName BaseApps -GitHubUserID $GitHubUserID -GitHubToken $GitHubToken
-	New-Item "$($PSDownload.fullname)\LabSetup.tmp" -ItemType file -Force | Out-Null
+	try {
+		Get-Service WinRM | Start-Service -Verbose
+		Write-Host "`n`n-----------------------------------" -ForegroundColor DarkCyan; Write-Host '[Installing]: ' -NoNewline -ForegroundColor Yellow; Write-Host "Base Apps`n" -ForegroundColor Cyan
+		Write-Host '[Checking] ' -NoNewline -ForegroundColor Yellow; Write-Host 'Pending Reboot: ' -ForegroundColor Cyan -NoNewline
+		if (Test-PendingReboot -ComputerName $env:COMPUTERNAME) {Invoke-Reboot}
+		else {Write-Host 'Not Required' -ForegroundColor Green}
+		Install-PSPackageManAppFromList -ListName BaseApps -GitHubUserID $GitHubUserID -GitHubToken $GitHubToken
+		New-Item "$($PSDownload.fullname)\LabSetup.tmp" -ItemType file -Force | Out-Null
+	} catch {Write-Warning "Error: Message:$($Error[0])"}
 }
 
 
