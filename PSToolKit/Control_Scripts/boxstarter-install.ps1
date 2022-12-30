fcbcfd86fede19f39fdc0c730794698b324656af
Pierre Smit <pierre.smit@outlook.com>
2022-12-30 21:29:01 +0200

    d

--- PSToolKit/Control_Scripts/boxstarter-install.ps1
+++ PSToolKit/Control_Scripts/boxstarter-install.ps1
@@ -1,20 +1,10 @@
-[scriptblock]$block = {
-	Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
-	Set-ExecutionPolicy Bypass -Scope LocalMachine -Force
-	[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-	Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1'))
-	Get-Boxstarter -Force
-}		
-
-Start-Process PowerShell -ArgumentList "-NoLogo -NoProfile -WindowStyle Maximized -ExecutionPolicy Bypass -Command (& {$($block)} )" -Wait
-
-
-[scriptblock]$block2 = {
-	https://boxstarter.org/package/url?https://raw.githubusercontent.com/smitpi/PSToolKit/master/PSToolKit/Control_Scripts/Initial-Setup.ps1
-
-}
-Start-Process PowerShell -ArgumentList "-NoLogo -NoProfile -WindowStyle Maximized -ExecutionPolicy Bypass -Command (& {$($block2)} )" -Wait
-
+Set-ExecutionPolicy Bypass -Scope CurrentUser -Force
+Set-ExecutionPolicy Bypass -Scope LocalMachine -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1'))
+Get-Boxstarter -Force
 
 #https://boxstarter.org/package/url?https://raw.githubusercontent.com/smitpi/PSToolKit/master/PSToolKit/Control_Scripts/Initial-Setup.ps1
 #Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/smitpi/PSToolKit/master/PSToolKit/Control_Scripts/Initial-Setup.ps1
+
+#  C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe --app=https://boxstarter.org/package/url?https://raw.githubusercontent.com/smitpi/PSToolKit/master/PSToolKit/Control_Scripts/Initial-Setup.ps1
